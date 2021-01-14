a5dfc0ee27ef16b1707e02ca268554d13c4750c4
hailong-lu <76936318+hailong-lu@users.noreply.github.com>
2021-01-14 09:56:33 +0800

    Update HelloWorld.py
    Refactor

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,12 +2,20 @@
 # Date   : 2020/1/4
 # Version: 0.1
 
+class Hello:
+    def say():
+        print("Hello")
 
-class HelloWorld:
+class HelloWorld(Hello):
+    def say():
+        __super__().hello():
+        print(" World ")
+
+class My(HelloWorld):
     def say(name):
-        print("Hello World ", name, "!")
-        
+        _super__().say()
+        print(" ", name)
         
 if __name__ == "__main__":
-    hw = HelloWorld()
+    hw = My()
     hw.say("Hailong")
